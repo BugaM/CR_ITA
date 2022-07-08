8cd0588e7f02b85ab8342cc1079f6e73690104a6
MateusCristoMelo <mateuscristomelo@hotmail.com>
2022-07-08 19:29:44 -0300

    PDF reader

--- front/src/components/dashboard/BasicInfos.js
+++ front/src/components/dashboard/BasicInfos.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react"
+import axios from 'axios'
 import styles from "./BasicInfos.module.css"
 
 function BasicInfos(props){
@@ -12,18 +13,29 @@ function BasicInfos(props){
 	};
 
     const handleSubmission = () => {
+        const formData = new FormData();
+
+		formData.append('File', selectedFile);
+
+		fetch(
+			'http://127.0.0.1:8000/core_api/get_data/',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
+			.then((response) => response.json())
 	};
 
 
     return (
         <div className={styles.BasicInfosContainer}>
-
             <div className={styles.BasicInfosName}>
-                Nome: {props.nome} 
+                Nome: {response.total_eletivas} 
             </div>
 
             <div className={styles.BasicInfosEng}>
-                Curso: {props.curso}
+                Curso: {response.curso}
             </div>
             
             <div className={styles.BasicInfosPDF}>
--- front/src/pages/Dashboard.js
+++ front/src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import Grades from '../components/dashboard/Grades';
 function Dashboard() {
     return (
       <div>
-        <BasicInfos nome = "Jack" curso = "Computacao" pdf = "Boletim"/> 
+        <BasicInfos /> 
         <Elective horasatuais = "12" horasnecessarias = "15" />
         <Grades nota = "9.9" conceito = "L" cr = "9.8"/>
       </div> 
