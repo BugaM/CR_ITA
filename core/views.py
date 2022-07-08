63e8f82a0edcea482ee6d9d99ea6649680fb296c
buga <marcelo.buga@hotmail.com>
2022-07-08 16:34:56 -0300

    adding cr calculator to django view

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from dataset.ScriptsMongoDB import ScriptsMongoDB
 
+from gradereportreader import GradeReportReader
 # Create your views here.
 
 def randomData(request):
@@ -14,3 +15,34 @@ def randomData(request):
       curso = doc['curso']
       return render(request, 'basic/test.html', {'nome' : nome, 'creditos': creditos,
                                                  'sigla' :sigla , 'curso' : curso})
+
+def get_cr(request):
+      scripts = ScriptsMongoDB()
+      path = "test_pdfs/test_buga.pdf"
+      reader = GradeReportReader(path)
+      grades = reader.get_grades()
+      scripts = ScriptsMongoDB()
+      total_creditos = 0
+      total_eletivas = 0
+      prof = reader.get_prof()
+
+
+      sum_creditos = 0
+
+      not_identified = []
+      media_simples = grades['grade'].mean()
+      for _, grade in grades.iterrows():
+            sigla = grade['courses']
+            data = scripts.get_data_find_one(collection_name='disciplinas', filter={'sigla': sigla})
+            if data:
+                  total_creditos += data['creditos']
+                  sum_creditos += data['creditos']*grade['grade']
+                  if 'ELETIVA' in data['curso']:
+                        total_eletivas += data['creditos']
+            else:
+                  not_identified.append(sigla)
+      cr = sum_creditos/total_creditos
+      return render(request, 'basic/test.html', {'cr' : cr, 'media_simples': media_simples,
+                                                 'curso' : prof, 'total_eletivas' : total_eletivas})      
+
+                        
