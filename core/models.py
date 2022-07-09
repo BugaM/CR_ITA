cb19933d58f0d1d9d0feae5595736fe0a897a6bc
buga <marcelo.buga@hotmail.com>
2022-07-09 18:28:28 -0300

    improving contact visualization for admin

--- core/models.py
+++ core/models.py
@@ -12,3 +12,6 @@ class Data(models.Model):
 class Contact (models.Model):
       name = models.CharField(max_length=100)
       text = models.CharField(max_length=1000)
+
+      def __str__(self):
+            return self.name
