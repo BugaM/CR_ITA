25e54152d2566eb7ce171cbd995de757297d2feb
buga <marcelo.buga@hotmail.com>
2022-07-09 18:17:18 -0300

    contact now saved on db

--- front/src/components/contactpage/ContactForm.js
+++ front/src/components/contactpage/ContactForm.js
@@ -1,9 +1,37 @@
 import styles from "./ContactForm.module.css"
+import React, { useState } from 'react';
 
 function ContactForm(){
+
+
+  const [writtenText, setWrittenText] = useState('');
+  const [writtenName, setWrittenName] = useState('');
+
+
+      
+  const updateName = (event) => {
+      setWrittenName(event.target.value)
+  };
+
+  const updateText = (event) => {
+    setWrittenText(event.target.value)
+  };
   
-  function SendEmail (){
+  async function SendEmail (){
+    console.log(writtenName)
+    console.log(writtenText)
+    let contact = {
+      name: writtenName,
+      text: writtenText
+    }
     alert("Email enviado! Agradecemos o contato")
+    fetch('http://127.0.0.1:8000/core_api/contact/', {
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(contact)
+    })
   }
 
     return (
@@ -12,9 +40,9 @@ function ContactForm(){
             <div style={{borderRadius: "20px", backgroundColor: "#183d6c" , padding : "20px"}}>
               <form onSubmit={SendEmail}>
                 <div style={{fontSize : "xx-large"}}> Remetente: </div>
-                <input type="text" style = {{width : "300px", textAlign : "left"}}></input>
+                <input type="text" onChange={updateName} style = {{width : "300px", textAlign : "left"}}></input>
                 <div style={{fontSize : "xx-large"}}> Mensagem: </div>
-                <textarea type="text" style = {{height : "200px", width : "300px", textAlign : "left"}}></textarea>
+                <textarea type="text" onChange={updateText} style = {{height : "200px", width : "300px", textAlign : "left"}}></textarea>
                 <div> <input type="submit"></input> </div>
               </form>
             </div>
