c39f179fa5f15e932ae14ba4feb78a800816c227
buga <marcelo.buga@hotmail.com>
2022-07-08 22:50:49 -0300

    making function update

--- front/src/components/dashboard/BasicInfos.js
+++ front/src/components/dashboard/BasicInfos.js
@@ -4,38 +4,38 @@ import styles from "./BasicInfos.module.css"
 
 function BasicInfos(props){
 
+
 	const [selectedFile, setSelectedFile] = useState();
 	const [isFilePicked, setIsFilePicked] = useState(false);
 
+
+    
 	const changeHandler = (event) => {
 		setSelectedFile(event.target.files[0]);
 		setIsFilePicked(true);
 	};
 
-    const handleSubmission = () => {
-        const formData = new FormData();
 
-		formData.append('File', selectedFile);
-
-		fetch(
-			'http://127.0.0.1:8000/core_api/get_data/',
-			{
-				method: 'POST',
-				body: formData,
-			}
-		)
-			.then((response) => response.json())
-	};
-
+    // const handleSubmission = () => {
+    //     fetch('http://127.0.0.1:8000/core_api/get_data/')
+    //     .then(response => response.json())
+    //     .then(results => {console.log('Success:', results);});
+	// };
+    // var response = {total_eletivas:2, curso: "-", }
+    // async function handleSubmission() {
+    //     response =  await (await fetch('http://127.0.0.1:8000/core_api/get_data/')).json()
+    //     console.log(response.curso)
+    //     setEletivas (response.total_eletivas)
+	// };
 
     return (
         <div className={styles.BasicInfosContainer}>
             <div className={styles.BasicInfosName}>
-                Nome: {response.total_eletivas} 
+                Nome: Marcelo
             </div>
 
-            <div className={styles.BasicInfosEng}>
-                Curso: {response.curso}
+            <div className={styles.BasicInfosEng} key={props.curso}>
+                Curso: {props.curso}
             </div>
             
             <div className={styles.BasicInfosPDF}>
@@ -43,7 +43,7 @@ function BasicInfos(props){
                 <div style = {{alignSelf: "center", height:"100%"}}>
                     <input type="file" name="pdf" onChange={changeHandler} style ={{color : "green", width : "290px"}}/>
                 </div>  
-                <div onClick={handleSubmission} style = {{cursor : "pointer", color : "green"}}> Enviar </div>
+                <div onClick={props.handleSub} style = {{cursor : "pointer", color : "green"}}> Enviar </div>
             </div>  
 
         </div>
--- front/src/pages/Dashboard.js
+++ front/src/pages/Dashboard.js
@@ -1,11 +1,21 @@
 import BasicInfos from '../components/dashboard/BasicInfos';
 import Elective from '../components/dashboard/Elective';
 import Grades from '../components/dashboard/Grades';
-
+import React, { useState } from 'react';
+  
 function Dashboard() {
+    const [curso, setCurso] = useState('-');
+    async function handleSubmission() {
+      
+      let response =  await (await fetch('http://127.0.0.1:8000/core_api/get_data/')).json()
+    
+      // setEletivas (response.total_eletivas)
+      setCurso (response.curso)
+    };
+    
     return (
       <div>
-        <BasicInfos /> 
+        <BasicInfos curso = {curso} key={curso} handleSub = {handleSubmission}/> 
         <Elective horasatuais = "12" horasnecessarias = "15" />
         <Grades nota = "9.9" conceito = "L" cr = "9.8"/>
       </div> 
